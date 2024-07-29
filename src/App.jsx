70f1fe3c3a7e67af255cf9dcd6fdaf4649ca9dd8
Vkxm <vikashkumar244@outlook.com>
2024-07-29 17:29:47 +0530

    leran app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  
+  return (<div>
+    <CustomButton count={count} setCount={setCount}></CustomButton>
+    <CustomButton count={count+2} setCount={setCount}></CustomButton>
+    <CustomDiv count={count} setCount={setCount}></CustomDiv>
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  </div>
   )
-}
 
+  
+}
+function CustomButton(props){
+  function handler(){
+    props.setCount(props.count+1);
+    }
+  return <button onClick={handler}> counter {props.count-1}</button>
+  
+}
+function CustomDiv(props){
+  function handler(){
+    props.setCount(props.count);
+    }
+  return <div onClick={handler}> counter {props.count}</div>
+  
+}
 export default App
